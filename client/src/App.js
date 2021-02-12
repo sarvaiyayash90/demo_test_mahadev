248396f683bf79478454d066506b5c35144e239b
sarvaiyayash90 <sarvaiyayash90@gmail.com>
2021-02-12 10:13:28 +0530

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,14 @@ import home from './Components/pages/home'
 function App() {
   const auth = localStorage.getItem('Token_Key')
   if (auth == null) {
-    return <Router><Route exact path="/login" component={login} /><Redirect to="/login" /></Router>
+    return <Router><Route exact path="/" component={login} /><Redirect to="/login" /></Router>
   }else{
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route exact path="/home" component={home} />
-          <Redirect from="/" to="/home" />
+          {/* <Redirect from="/" to="/home" /> */}
         </Switch>
       </Router>
     </div>
